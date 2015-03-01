a975328a37cb3115d3fe2dfab61e8fc3a75826da
jluk <jluk.gt@gmail.com>
2015-02-28 21:05:18 -0500

    add chapter 7

--- src/main/scala/net/s_mach/sfti/Chapter7.scala
+++ src/main/scala/net/s_mach/sfti/Chapter7.scala
@@ -6,9 +6,33 @@ import scala.collection.mutable
  * author:JustinLuk
  * date:2/22/15
  */
+
+package com{
+
+  object authorTester {
+    def isAuthor(name: String): Boolean = {
+      if (name != "Horstmann") true
+      else false
+    }
+  }
+  package horstmann{
+    object Jack{
+      if (authorTester.isAuthor("Jack")){
+        println("Jack is not Horstmann")
+      }
+    }
+  }
+}
+
 object Chapter7 {
   import java.util.{ HashMap => JavaHashMap }
   import scala.collection.mutable.HashMap
+
+  /*
+  7.5 meaning of private[com] def giveRaise(rate:Double) allows the visibility of giveRaise to be seen by the package
+  com
+   */
+
 
   //7.6 Copy all elements from Java hashmap and turn into a scala hashmap
   def convertMap(jh: JavaHashMap[Int,String]) : HashMap[Int,String] = {
@@ -23,6 +47,7 @@ object Chapter7 {
     sh
   }
 
+    //7.8 namespaces can collide and you probably won't use all the packages
 
 
 }
